6ef241aff87eb11ba2edc0d4a5773e2da3b7a961
khoadpm245442-glitch <khoa_dpm245442@student.agu.edu.vn>
2025-09-18 10:02:10 +0700

    Giai bai tap TH chuong 2

---
+++ Chuong2_Cau6.py
@@ -0,0 +1,8 @@
+#Trả lời câu 6 chương 2
+"""
+Câu 6: Trình bày ý nghĩa toán tử /, //, %, **, and, or, is.
+1./ (Phép chia) dùng để trả về kết quả của phép chia thông thường, bao gồm cả phần thập phân
+2.// (Chia lấy phần nguyên) dùng để trả về phần nguyên của thương số, làm tròn xuống số nguyên gần nhất
+3.% (Chia lấy phần dư) dùng để trả về phần dư của phép chia
+4.** (Lũy thừa) dùng để tính lũy thừa của một số cho một số mũ
+"""
---
+++ Chuong2_Cau9.py
@@ -0,0 +1,85 @@
+#Trả lời câu hỏi số 9 chương 2
+import math
+
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+
+#Câu (a)
+kq1 = i1 + (i2 * i3)
+print("Ket qua cau A la: ", kq1)
+#Câu (b)
+kq2 = i1 * (i2 + i3)
+print("Ket qua cua cau B la: ", kq1)
+#Cau (C)
+kq3 = i1 / (i2+i3)
+print("Ket qua cua cau C la: ", kq3)
+#Cau (D)
+kq4 = i1 // (i2 + i3)
+print("Ket qua cua cau D la: ", kq4)
+#Cau (E)
+kq5 = i1 / i2 + i3
+print("Ket qua cua cau E la: ", kq5)
+#Cau (F)
+kq6 = i1 // i2 + i3
+print("Ket qua cua cau F la: ", kq6)
+#Cau (G)
+kq7 = 3 + 4 + 5 /3
+print("Ket qua cua cau G la: ", kq7)
+#Cau (H)
+kq8 = 3+4+5 //3
+print("Ket qua cua cau H la: ", kq8)
+#Cau (I)
+kq9 = (3 + 4 + 5) / 3
+print("Ket qua cua cau I la: ", kq9)
+#Cau (J)
+kq10 = (3 + 4 + 5) // 3
+print("Ket qua cua cau J la: ", kq10)
+#Cau (K)
+kqA =  d1 + (d2 * d3)
+print("Ket qua cua cau K la: ", kqA)
+#Cau (L)
+kqB = d1 + d2 * d3
+print("Ket qua cua cau L la: ", kqB)
+#Cau (m) 
+kqC = d1 / d2 - d3
+print("Ket qua cua cau M la: ", kqC)
+# Cau (n) 
+kqD = d1 / (d2 - d3)
+print("Ket qua cua cau N la: ", kqD)
+#Cau (o) 
+kqE=d1 + d2 + d3 / 3
+print("Ket qua cua cau O la: ", kqE)
+#Cau (p) 
+kqF= (d1 + d2 + d3) / 3
+print("Ket qua cua cau P la: ", kqF)
+#Cau (q) 
+kqG = d1 + d2 + (d3 / 3)
+print("Ket qua cua cau Q la: ", kqG)
+#Cau (r) 
+kqH=3 * (d1 + d2) * (d1 - d3)
+print("Ket qua cua cau R la: ", kqH)
+
+"""
+Ket qua cau A la:  -13
+Ket qua cua cau B la:  -13
+Ket qua cua cau C la:  1.0
+Ket qua cua cau D la:  1
+Ket qua cua cau E la:  -2.6
+Ket qua cua cau F la:  -3
+Ket qua cua cau G la:  8.666666666666666
+Ket qua cua cau H la:  8
+Ket qua cua cau I la:  4.0
+Ket qua cua cau J la:  4
+Ket qua cua cau K la:  -0.5
+Ket qua cua cau L la:  -0.5
+Ket qua cua cau M la:  0.9
+Ket qua cua cau N la:  0.36363636363636365
+Ket qua cua cau O la:  6.833333333333333
+Ket qua cua cau P la:  2.1666666666666665
+Ket qua cua cau Q la:  6.833333333333333
+Ket qua cua cau R la:  52.5
+"""
